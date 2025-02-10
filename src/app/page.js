ab62266c190f2bb33ae7a0bd6728533acda147f3
nandakishore <nandakishore695@gmail.com>
2025-02-10 17:20:33 +0530

    todolist

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,77 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FaRegEdit, } from "react-icons/fa";
+import { MdOutlineDeleteSweep } from "react-icons/md";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [toDoList, setToDoList] = useState([
+    { task: "Do My Homework" },
+    { task: "Do My Bike Clean" }
+  ]);
+  const [newToDoList, setNewToDoList] = useState("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const handleInputChange = (event) => {
+    setNewToDoList(event.target.value);
+  }
+
+  const handleAdd = () => {
+    const existingName = toDoList.some((item) => item.task === newToDoList);
+    if (existingName) {
+      alert('Name already exists!');
+      return;
+    }
+    else if (!newToDoList) { alert("You must write something!"); }
+    else if (newToDoList.length <= 3) { alert("You must write greater then three characters!"); }
+    else {
+      setToDoList([...toDoList, { task: newToDoList }]);
+      setNewToDoList("");
+    }
+  }
+
+  const handleEdit = (task) => {
+    const editList = toDoList.find((item) => item.task === task);
+    setNewToDoList(editList.task);
+  }
+
+  const handleRevmove = (task) => {
+    const updatedList = toDoList.filter((item) => item.task !== task);
+    setToDoList(updatedList);
+  }
+
+  return (
+    <main className="container">
+      <div className="text-center">
+        <h1 className="text-black fw-bold text-center my-4">My Todo List</h1>
+        <div className="d-grid grid lg-col-6 mx-auto ">
+          <div className="input-group mb-3">
+            <input className="form-control" name="addtodolist" type="text" placeholder="Add a todo..." onChange={handleInputChange} value={newToDoList} />
+            <button className="btn btn-primary w-full" type="button" onClick={handleAdd}>Add Task <FaPlus className="ml-4" size={14} onClick={handleAdd} />
+            </button>
+          </div>
+        </div >
+      </div>
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">S.No</th>
+              <th scope="col">Task</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {toDoList.map((todo, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{todo.task}</td>
+                <td><FaRegEdit size={22} className="ml-4 cursor-pointer" onClick={() => handleEdit(todo.task)} /><MdOutlineDeleteSweep size={28} className="ml-4 " onClick={() => handleRevmove(todo.task)} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
   );
 }
